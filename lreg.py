1c9d9950db17d658e4166640f7c9366451f5252e
quantumke <nguruben@gmail.com>
2016-09-21 07:38:50 +0300

    used make_moons to make dataset and plt using matplotlib

--- lreg.py
+++ lreg.py
@@ -6,6 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import *
 
+class linearregression():
+    def __init__(self, *args, **kwargs):
+        self.author="Benon Mburu"
+        self.load_dataset()
+    def seeder_method(self):
+        np.random.seed(0)
+        X,y=sklearn.datasets.make_moons(200, noise=0.20)
+        plt.scatter(X[:,0],X[:,1],s=14, c=y, cmap=plt.cm.Spectral)
+        plt.show()
 
 app =linearregression()
 
