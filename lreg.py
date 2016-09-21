7845eb6a69dd5a8a84153aa405bccafaf638ae84
quantumke <nguruben@gmail.com>
2016-09-21 07:37:44 +0300

    import all required libs

---
+++ lreg.py
@@ -0,0 +1,11 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import sklearn
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
+
+
+app =linearregression()
+
