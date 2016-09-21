3d7c85728bc5d32c84d2edf7e7afe206c6992f9a
quantumke <nguruben@gmail.com>
2016-09-21 07:39:42 +0300

    create the learning models

--- lreg.py
+++ lreg.py
@@ -15,6 +15,21 @@ class linearregression():
         X,y=sklearn.datasets.make_moons(200, noise=0.20)
         plt.scatter(X[:,0],X[:,1],s=14, c=y, cmap=plt.cm.Spectral)
         plt.show()
+   def load_dataset(self):
+        seed=7
+        np.random.seed(seed)
+        dataset=np.loadtxt("games.csv", delimiter=",")
+        X = dataset[:,0:8]
+        Y = dataset[:,8]
+        model=Sequential()
+            #fuuly conected layers are defined using Dense class
+            #first argument specifies number of neurons
+            #second argument specify the initialization method
+            #third argument specifies activation function
+        model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+        model.add(Dense(8, init='uniform', activation='relu'))
+        model.add(Dense(1, init='uniform', activation='sigmoid'))
+
 
 app =linearregression()
 
